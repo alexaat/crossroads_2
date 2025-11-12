8ec363123a340f1b13db8e0717a473d69f3f49fd
Aliaksei Vidaseu <alexaat@yahoo.co.uk>
2025-11-12 19:19:04 +0000

    key listenets

--- src/controller.rs
+++ src/controller.rs
@@ -1,6 +1,11 @@
+use crate::model::Destination;
+use crate::model::VehicleType;
+use crate::model::CARDINAL;
 use crate::Model;
 use crate::View;
+use rand::Rng;
 use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 pub struct Controller {
     model: Model,
     view: View,
@@ -17,9 +22,82 @@ impl Controller {
         }
     }
 
-    pub fn key_down(&mut self, event: Event) {}
+    pub fn key_down(&mut self, event: Event) {
+        match event {
+            Event::KeyDown {
+                keycode: Some(Keycode::Down),
+                ..
+            } => self.model.spawn_car(
+                CARDINAL::NORTH,
+                Controller::get_random_destination(),
+                Controller::get_random_type(),
+            ),
+            Event::KeyDown {
+                keycode: Some(Keycode::Up),
+                ..
+            } => self.model.spawn_car(
+                CARDINAL::SOUTH,
+                Controller::get_random_destination(),
+                Controller::get_random_type(),
+            ),
+            Event::KeyDown {
+                keycode: Some(Keycode::Left),
+                ..
+            } => self.model.spawn_car(
+                CARDINAL::EAST,
+                Controller::get_random_destination(),
+                Controller::get_random_type(),
+            ),
+            Event::KeyDown {
+                keycode: Some(Keycode::Right),
+                ..
+            } => self.model.spawn_car(
+                CARDINAL::WEST,
+                Controller::get_random_destination(),
+                Controller::get_random_type(),
+            ),
+            Event::KeyDown {
+                keycode: Some(Keycode::R),
+                ..
+            } => self.model.spawn_car(
+                Controller::get_random_origin(),
+                Controller::get_random_destination(),
+                Controller::get_random_type(),
+            ),
+            _ => {}
+        }
+    }
 
     pub fn tick(&mut self) {}
+
+    pub fn get_random_destination() -> Destination {
+        let r = rand::thread_rng().gen_range(0..3);
+        match r {
+            0 => Destination::LEFT,
+            1 => Destination::AHEAD,
+            _ => Destination::RIGHT,
+        }
+    }
+
+    pub fn get_random_origin() -> CARDINAL {
+        let r = rand::thread_rng().gen_range(0..4);
+        match r {
+            0 => CARDINAL::WEST,
+            1 => CARDINAL::NORTH,
+            2 => CARDINAL::EAST,
+            _ => CARDINAL::SOUTH,
+        }
+    }
+
+    pub fn get_random_type() -> VehicleType {
+        //random vehicle type
+        let r = rand::thread_rng().gen_range(0..3);
+        match r {
+            0 => VehicleType::BlueCar,
+            1 => VehicleType::GreenCar,
+            _ => VehicleType::RedCar,
+        }
+    }
 }
 
 #[derive(PartialEq)]
--- src/model.rs
+++ src/model.rs
@@ -3,4 +3,34 @@ impl Model {
     pub fn new() -> Self {
         Self {}
     }
+
+    pub fn spawn_car(
+        &mut self,
+        origin: CARDINAL,
+        destination: Destination,
+        vehicle_type: VehicleType,
+    ) {
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum CARDINAL {
+    SOUTH,
+    NORTH,
+    WEST,
+    EAST,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Destination {
+    LEFT,
+    AHEAD,
+    RIGHT,
+}
+
+#[derive(Debug, Clone)]
+pub enum VehicleType {
+    RedCar,
+    BlueCar,
+    GreenCar,
 }
